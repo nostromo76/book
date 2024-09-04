0f7d7f686bd99dc118e33cc813936d32b2fa89c9
nostromo76 <ivans1976@gmail.com>
2024-09-04 17:32:02 +0200

    Users page for coments and posts images,; test1 insert into db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index(){
 
         return view('home.index');
+    }
+
+  public function upload(Request $request){
+
+    $data= new Post;
+    $data->username=Auth::user()->name;
+    $data->description=$request->description;
+    $data->save();
+    return redirect()->back();
+
+
     }
 }
